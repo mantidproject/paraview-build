f8d5e4398630d1254c19b9649f87caf4dcbbe36b
Steven Hahn <hahnse@ornl.gov>
2015-11-30 14:58:21 -0500

    Update linux.cmake

--- srcbuild/linux.cmake
+++ srcbuild/linux.cmake
@@ -1,4 +1,3 @@
 set(VTK_USE_SYSTEM_JPEG ON CACHE PATH "")
-#ubuntu < 15.04, RHEL 7 and Fedora 23 come with jsoncpp 0.6, which is too old. 
-set(VTK_USE_SYSTEM_JSONCPP OFF CACHE PATH "")
-
+# Mixing system and vtk jsoncpp is creating issue with colormaps. 
+set(VTK_USE_SYSTEM_JSONCPP ON CACHE PATH "")
