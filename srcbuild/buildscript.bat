ccc6c4dcc48c75150a82df6e0e37aeedb1447d1d
Steven Hahn <hahnse@ornl.gov>
2016-01-12 20:31:11 -0500

    Remove patch now in VTK master.

--- srcbuild/buildscript.bat
+++ srcbuild/buildscript.bat
@@ -74,8 +74,6 @@ call:fetch-paraview
 :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: 
 :: Apply patches not yet in ParaView source 
 :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: 
-cd %SRC_DIR%\%PARAVIEW_SRC%\VTK 
-"%GitCmd%" apply %SCRIPT_DIR%\patches\vtk_jsoncpp.patch
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :: Build ParaView
