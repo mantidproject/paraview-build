827287cf79f5012cece08f3ef1fed6f2c91e1670
Steven Hahn <hahnse@ornl.gov>
2016-05-24 18:53:01 -0400

    Update linux.cmake

--- linux.cmake
+++ linux.cmake
@@ -1,3 +1,2 @@
 set(VTK_USE_SYSTEM_JPEG ON CACHE PATH "")
-#ubuntu < 15.04, RHEL 7 and Fedora 23 come with jsoncpp 0.6, which is too old. 
-set(VTK_USE_SYSTEM_JSONCPP OFF CACHE PATH "")
+set(VTK_USE_SYSTEM_JSONCPP ON CACHE PATH "")
