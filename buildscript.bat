dacb9d57f802685eec57568b4b3511e4d9b46840
Steven Hahn <hahnse@ornl.gov>
2016-09-26 10:58:55 -0400

    Remove patch already added upstream

--- buildscript.bat
+++ buildscript.bat
@@ -75,9 +75,7 @@ call:fetch-paraview
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 cd %SRC_DIR%\%PARAVIEW_SRC%
 "%GitCmd%" apply --whitespace=fix %SCRIPT_DIR%\patches\vtk_override.patch
-"%GitCmd%" apply --whitespace=fix %SCRIPT_DIR%\patches\hidden_symbol_visibility.patch
-cd %SRC_DIR%\%PARAVIEW_SRC%\VTK
-"%GitCmd%" apply --whitespace=fix %SCRIPT_DIR%\patches\FasterIsCellVisible.patch
+:: cd %SRC_DIR%\%PARAVIEW_SRC%\VTK
 if ERRORLEVEL 1 exit /B %ERRORLEVEL%
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
