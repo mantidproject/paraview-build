b7e5282257016d57300ee66fb49428c0a165ef3a
Steven Hahn <hahnse@ornl.gov>
2016-09-26 10:25:15 -0400

    Update common.cmake

--- common.cmake
+++ common.cmake
@@ -5,6 +5,7 @@ set (BUILD_TESTING OFF CACHE BOOL "")
 set (BUILD_EXAMPLES OFF CACHE BOOL "")
 set (CMAKE_CXX_STANDARD 14 CACHE STRING "")
 set (CMAKE_CXX_STANDARD_REQUIRED 11 CACHE STRING "")
+set (VTK_USE_CXX11_FEATURES ON CACHE BOOL "")
 set (CMAKE_BUILD_TYPE Release CACHE STRING "")
 
 ###############################################################################
