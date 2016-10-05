4db8ad9b39cbf7a7bbab23a245d1a73263fe36cc
Steven Hahn <hahnse@ornl.gov>
2016-10-05 17:55:37 -0400

    Update msvc-2015.cmake

--- msvc-2015.cmake
+++ msvc-2015.cmake
@@ -35,10 +35,9 @@ set (JSONCPP_LIBRARY_DEBUG ${BASE_LIB_DIR}/jsoncpp_d.lib CACHE FILEPATH "")
 # # hdf5
 set (HDF5_INCLUDE_DIRS "${BASE_INCLUDE_DIR}" CACHE PATH "")
 set (HDF5_C_LIBRARY ${BASE_LIB_DIR}/hdf5.lib CACHE FILEPATH "")
-set (HDF5_HL_LIBRARY ${BASE_LIB_DIR}/hdf5_hl.lib CACHE FILEPATH "")
+set (HDF5_C_HL_LIBRARY ${BASE_LIB_DIR}/hdf5_hl.lib CACHE FILEPATH "")
 set (HDF5_CXX_LIBRARY ${BASE_LIB_DIR}/hdf5_cpp.lib CACHE FILEPATH "")
 set (HDF5_HL_CPP_LIBRARY ${BASE_LIB_DIR}/hdf5_hl_cpp.lib CACHE FILEPATH "")
-set (HDF5_LIBRARIES "${HDF5_C_LIBRARY};${HDF5_HL_LIBRARY};${HDF5_CXX_LIBRARY};${HDF5_HL_CPP_LIBRARY}" CACHE PATH "")
 
 ###############################################################################
 # System libraries
