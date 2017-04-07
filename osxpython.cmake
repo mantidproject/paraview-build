d26f600ca8399cd8c9fdacd7afdec633a32a2f25
Steven Hahn <hahnse@ornl.gov>
2017-04-06 20:48:57 -0400

    use homebrew python on El Capitan

---
+++ osxpython.cmake
@@ -0,0 +1,2 @@
+set( PYTHON_INCLUDE_DIR "/usr/local/opt/python/Frameworks/Python.framework/Versions/2.7/include/python2.7" CACHE PATH "")
+set( PYTHON_LIBRARY "/usr/local/opt/python/Frameworks/Python.framework/Versions/2.7/lib/libpython2.7.dylib" CACHE FILEPATH "")
