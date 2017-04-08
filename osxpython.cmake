5ff379ce61bb3b129b3fe94e942a97280de848eb
Steven Hahn <hahnse@ornl.gov>
2017-04-08 09:15:54 -0400

    fixup python3.

--- osxpython.cmake
+++ osxpython.cmake
@@ -1,2 +1,3 @@
-set( PYTHON_INCLUDE_DIR "/usr/local/opt/python/Frameworks/Python.framework/Versions/2.7/include/python2.7" CACHE PATH "")
-set( PYTHON_LIBRARY "/usr/local/opt/python/Frameworks/Python.framework/Versions/2.7/lib/libpython2.7.dylib" CACHE FILEPATH "")
+set( PYTHON_EXECUTABLE /usr/local/bin/python CACHE PATH "")
+set( PYTHON_INCLUDE_DIR /usr/local/opt/python/Frameworks/Python.framework/Versions/2.7/include/python2.7 CACHE PATH "")
+set( PYTHON_LIBRARY /usr/local/opt/python/Frameworks/Python.framework/Versions/2.7/lib/libpython2.7.dylib CACHE FILEPATH "")
