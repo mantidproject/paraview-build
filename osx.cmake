d3c2c8a114caa851a7f5df21b7d65aee35dc0ab2
Steven Hahn <hahnse@ornl.gov>
2017-06-29 13:33:47 -0400

    Workaround high jsoncpp requirement.

--- osx.cmake
+++ osx.cmake
@@ -1,5 +1,4 @@
 set(CMAKE_CXX_FLAGS "-Wno-inconsistent-missing-override" CACHE STRING "")
 set(VTK_USE_SYSTEM_JPEG ON CACHE PATH "")
-set(VTK_USE_SYSTEM_JSONCPP ON CACHE BOOL "")
 set(HDF5_ROOT /usr/local/opt/hdf5 CACHE PATH "")
 
