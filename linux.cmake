61102269854f40992c1a71c933673ea8c8a9a6b6
Steven Hahn <hahnse@ornl.gov>
2018-07-19 17:07:51 -0400

    Fix linking on Ubuntu 18.04

--- linux.cmake
+++ linux.cmake
@@ -1,1 +1,4 @@
+set(CMAKE_MODULE_LINKER_FLAGS "-Wl,--disable-new-dtags" CACHE STRING "")
+set(CMAKE_EXE_LINKER_FLAGS "-Wl,--disable-new-dtags" CACHE STRING "")
+set(CMAKE_SHARED_LINKER_FLAGS "-Wl,--disable-new-dtags" CACHE STRING "")
 set(VTK_USE_SYSTEM_JPEG ON CACHE PATH "")
