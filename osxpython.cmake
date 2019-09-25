fb2aaed5498d432bf3426adeecf25511db783b47
Martyn Gigg <martyn.gigg@stfc.ac.uk>
2019-09-25 10:01:48 +0100

    Use versioned python executable for macOS

--- osxpython.cmake
+++ osxpython.cmake
@@ -1,3 +1,3 @@
-set( PYTHON_EXECUTABLE /usr/local/bin/python CACHE PATH "")
-set( PYTHON_INCLUDE_DIR /usr/local/opt/python/Frameworks/Python.framework/Versions/2.7/include/python2.7 CACHE PATH "")
-set( PYTHON_LIBRARY /usr/local/opt/python/Frameworks/Python.framework/Versions/2.7/lib/libpython2.7.dylib CACHE FILEPATH "")
+set( PYTHON_EXECUTABLE /usr/local/bin/python2 CACHE PATH "")
+set( PYTHON_INCLUDE_DIR /usr/local/opt/python@2/Frameworks/Python.framework/Versions/2.7/include/python2.7 CACHE PATH "")
+set( PYTHON_LIBRARY /usr/local/opt/python@2/Frameworks/Python.framework/Versions/2.7/lib/libpython2.7.dylib CACHE FILEPATH "")
