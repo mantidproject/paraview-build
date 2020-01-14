341c6ac066842ec5bb4b985cdd6ddfc80aa8dc11
Martyn Gigg <martyn.gigg@gmail.com>
2020-01-14 14:07:00 +0000

    Add Python 3 build support for macOS

--- osxpython3.cmake
+++ osxpython3.cmake
@@ -1,3 +1,3 @@
 set( PYTHON_EXECUTABLE /usr/local/bin/python3 CACHE FILEPATH "")
-set( PYTHON_INCLUDE_DIR /usr/local/opt/python3/Frameworks/Python.framework/Versions/3.6/include/python3.6m CACHE PATH "")
-set( PYTHON_LIBRARY /usr/local/opt/python3/Frameworks/Python.framework/Versions/3.6/lib/libpython3.6m.dylib CACHE FILEPATH "")
+set( PYTHON_INCLUDE_DIR /usr/local/opt/python3/Frameworks/Python.framework/Versions/3.7/include/python3.7m CACHE PATH "")
+set( PYTHON_LIBRARY /usr/local/opt/python3/Frameworks/Python.framework/Versions/3.7/lib/libpython3.7m.dylib CACHE FILEPATH "")
