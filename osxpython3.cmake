44524ec04ac20a4874b14b596e3dcfe8f6b0f8d3
Martyn Gigg <martyn.gigg@stfc.ac.uk>
2020-06-01 08:53:47 +0100

    Update macOS Python 3 configuration

--- osxpython3.cmake
+++ osxpython3.cmake
@@ -1,3 +1,3 @@
-set( PYTHON_EXECUTABLE /usr/local/bin/python3 CACHE FILEPATH "")
-set( PYTHON_INCLUDE_DIR /usr/local/opt/python3/Frameworks/Python.framework/Versions/3.7/include/python3.7m CACHE PATH "")
-set( PYTHON_LIBRARY /usr/local/opt/python3/Frameworks/Python.framework/Versions/3.7/lib/libpython3.7m.dylib CACHE FILEPATH "")
+set( PYTHON_EXECUTABLE /usr/local/opt/python@3.8/bin CACHE FILEPATH "")
+set( PYTHON_INCLUDE_DIR /usr/local/opt/python@3.8/Frameworks/Python.framework/Versions/3.8/include/python3.8 CACHE PATH "")
+set( PYTHON_LIBRARY /usr/local/opt/python@3.8/Frameworks/Python.framework/Versions/3.8/lib/libpython3.8.dylib CACHE FILEPATH "")
